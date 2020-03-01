deda016a66d82f19e3c2822b44e54ee07d5fad27
hrtarposhyan <hrtarposhyan@gmail.com>
2020-03-01 15:23:22 -0800

    Add users in DB

--- CodeFirstEF/CodeFirstEF/Program.cs
+++ CodeFirstEF/CodeFirstEF/Program.cs
@@ -10,6 +10,24 @@ namespace CodeFirstEF
     {
         static void Main(string[] args)
         {
+            using(UserContext db=new UserContext())
+            {
+                User user1 = new User { Name = "Vahan", Age = 78 };
+                User user2 = new User { Name = "Varduhi", Age = 56 };
+
+                // Add users in DB
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+
+                var users = db.Users;
+                foreach (var u in users)
+                {
+                    Console.WriteLine("{0}.{1} - {2}", u.Id, u.Name, u.Age);
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
