5170644c1bc9e915795c54ac738529424d4f37ae
hrtarposhyan <hrtarposhyan@gmail.com>
2020-03-04 04:11:37 -0800

    Add property User Adress

---
+++ CodeFirstEF/CodeFirstEF/Migrations/202003041210297_Add_Address.cs
@@ -0,0 +1,18 @@
+﻿namespace CodeFirstEF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Add_Address : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Users", "Address", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Users", "Address");
+        }
+    }
+}
--- CodeFirstEF/CodeFirstEF/User.cs
+++ CodeFirstEF/CodeFirstEF/User.cs
@@ -11,5 +11,6 @@ namespace CodeFirstEF
         public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
+        public string Address { get; set; }
     }
 }
